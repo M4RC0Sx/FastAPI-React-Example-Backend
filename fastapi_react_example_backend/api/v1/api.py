741f816d5d71b0e1d0c530d4a7c0101364cd7415
M4RC0Sx <marcos3as3@gmail.com>
2025-07-06 23:41:55 +0200

    feat(api): add users api router

--- fastapi_react_example_backend/api/v1/api.py
+++ fastapi_react_example_backend/api/v1/api.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from fastapi import APIRouter
 
 from . import auth  # noqa: TID252
+from . import users  # noqa: TID252
 
 
 router = APIRouter()
 
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
+router.include_router(users.router, prefix="/users", tags=["users"])
